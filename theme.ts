9470dc284347cfd1714b3dfa0c0bb2f8c693a573
Willie Xu <ity@keemail.me>
2022-03-02 05:41:30 -1100

    Update theme.ts

--- theme.ts
+++ theme.ts
@@ -1,31 +1,47 @@
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
-import { prismDarkTheme, prismLightTheme } from "./prism";
 
-const styles = {
-  global: (props) => ({
-    body: {
-      bg: mode("#f8f8f5", "#202023")(props),
-      fontFamily:
-        '-ui-monospace,SFMono-Regular,"SF Mono, Menlo",Consolas,Liberation Mono,monospace',
-      lineHeight: 'base',
+export default extendTheme({
+    colors: {
+        lingva: {
+            50: "#F7FAFC",
+            100: "#EDF2F7",
+            200: "#E2E8F0",
+            300: "#CBD5E0",
+            400: "#A0AEC0",
+            500: "#718096",
+            600: "#4A5568",
+            700: "#2D3748",
+            800: "#1A202C",
+            900: "#171923"
+        }
     },
-  }),
-};
-
-const components = {
-  Link: {
-    basestyle: (props) => ({
-      color: mode("#3d7aed", "#ff63c3")(props),
-      textUnderlinOffset: 3,
-    }),
-  },
-};
-
-
-const theme = extendTheme({
-  components,
-  styles,
+    config: {
+        initialColorMode: process.env["DEFAULT_DARK_THEME"] === "true" ? "dark" : "light",
+        useSystemColorMode: true
+    },
+    components: {
+        Textarea: {
+            variants: {
+                outline: props => ({
+                    borderColor: mode("lingva.500", "lingva.200")(props),
+                    _hover: {
+                        borderColor: mode("lingva.700", "lingva.400")(props),
+                    },
+                    _readOnly: {
+                        userSelect: "auto"
+                    }
+                })
+            }
+        },
+        Select: {
+            variants: {
+                flushed: props => ({
+                    field: {
+                        borderColor: mode("lingva.500", "lingva.200")(props)
+                    }
+                })
+            }
+        }
+    }
 });
-
-export default theme;

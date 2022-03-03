cd60d94471608b5dad518b251263ccf4d0333864
Willie Xu <ity@keemail.me>
2022-03-03 22:32:54 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,22 +13,21 @@ const Footer: FC<Props> = (props) => (
         w="full"
         p={3}
         fontSize={["sm", null, "md"]}
-        direction={["column", null, "row"]}
+        direction="column"
         justify="center"
         align="center"
         spacing={[1, null, 2]}
         {...props}
     >
 
-        <Link href="https://github.com/xuwillie/trans/blob/main/LICENSE" isExternal={true} my={1}>
+        <Link href="https://github.com/xuwillie/trans/blob/main/LICENSE" isExternal={true} mt={1}>
             <Text as="span">&#169; {new Date().getFullYear()} Wll Translate</Text>
         </Link>
   <Link href="https://github.com/xuwillie/trans/" isExternal={true} mt={1}>
             <Text as="span">View Source</Text>
-        </Link>
-        <Text as="span" display={["none", null, "unset"]}>·</Text>
+        </Link> 
 <Link href="https://collection.wll.moe" isExternal={true} mt={1}>
-            <Text as="span">More Great OpenSource Software</Text>
+            <Text as="span">Recommend Software</Text>
         </Link>
         <Link href="https://www.gnu.org/licenses/agpl-3.0.html" isExternal={true} mt={1}>
             <Text as="span">Licensed under AGPLv3</Text>
